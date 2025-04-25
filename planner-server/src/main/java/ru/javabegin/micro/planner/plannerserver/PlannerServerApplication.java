60827bcdb6e0600a31f565f7eb2107bc90c86d06
Roman <zdybel2002@gmail.com>
2025-04-26 00:51:48 +0300

    Create Planner Server

---
+++ planner-server/src/main/java/ru/javabegin/micro/planner/plannerserver/PlannerServerApplication.java
@@ -0,0 +1,15 @@
+package ru.javabegin.micro.planner.plannerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PlannerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerServerApplication.class, args);
+	}
+
+}
